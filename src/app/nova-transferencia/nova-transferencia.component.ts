312868ad3c43e0636826149e6f20c81b925866ea
Ana Clara <anaclaranoronha.m@gmail.com>
2022-12-15 14:15:53 -0300

    Aula 6

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,37 @@
+import { Transferencia } from './../models/transferencia.model';
+import { TransferenciaService } from './../services/transferencia.service';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+export class NovaTransferenciaComponent {
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number = 0;
+  destino: number = 0;
+
+  constructor(private service: TransferenciaService){}
+
+  transferir(){
+    alert(`Solicitada Nova Transferencia: Valor: ${this.valor} | Destino: ${this.destino}`)
+
+    const valorEmitir: Transferencia = {valor: this.valor, destino: this.destino};
+
+    this.service.adicionar(valorEmitir).subscribe(resultado => {
+      console.log(resultado);
+      this.limparCampos();
+    }, 
+      (error )=> console.error(error)
+    );
+
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
